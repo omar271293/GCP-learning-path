e67656177c3b5a75957d9f79fa8580fd507e0126
Omar Vargas <omar271293@gmail.com>
2026-05-11 22:45:01 -0600

    validacion sobre resultados academicos

--- prueba.py
+++ prueba.py
@@ -1,1 +1,8 @@
-print("ya quedo, mi primer script")
+# Usamos input() para que el usuario nos de el dato
+entrada = input("Ingresa tu calificación: ")
+calificacion = int(entrada) # Convertimos el texto a número
+
+if calificacion >= 70:
+    print("¡Felicidades, Omar! Estás aprobado.")
+else:
+    print("Oh Oh, toca repasar.")
